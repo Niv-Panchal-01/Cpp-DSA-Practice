349dfa401acc41613eef4dba6e0ffa4b55d05633
Niv Panchal <nivpanchal2122@gmail.com>
2026-06-24 14:12:59 +0530

    added square of number pattern

--- patterns/numbersPatterns.cpp
+++ patterns/numbersPatterns.cpp
@@ -53,10 +53,34 @@ void floyedsTriangle(int n) {
    }
 }
 
+void numbersSqaure(int n) {
+   for (int i = 0; i < 2 * n - 1; i++) {
+      int val = n;
+      for (int k = 0; k < 2 * n - 1; k++) {
+         cout << val << ' ';
+         if (i < n) {
+            if (i > k) {
+               val--;
+            } else if (k >= 2 * n - i - 2) {
+               val++;
+            }
+         } else {
+            if (k < 2 * n - i - 2) {
+               val--;
+            } else if (i <= k) {
+               val++;
+            }
+         }
+      }
+      cout << endl;
+   }
+}
+
 int main() {
    int n;
    cout << "Enter number of rows : ";
    cin >> n;
 
+   numbersSqaure(n);
    return 0;
 }
