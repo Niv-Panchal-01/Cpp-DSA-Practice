69d5921607c592e3de4bc1801c50591ef2628afd
Niv Panchal <nivpanchal2122@gmail.com>
2026-07-01 23:04:15 +0530

    Another main function added for Uppercase Alphabets

--- hashing/basic_practices/totalAppearencesOfCharacters.cpp
+++ hashing/basic_practices/totalAppearencesOfCharacters.cpp
@@ -1,8 +1,46 @@
 #include <iostream>
 using namespace std;
 
+// This main function is only valid for uppercase alphabet
+// int main() {
+//    cout << "NOTE: THIS IS ONLY FOR LOWERCASE ALPHABETS !!" << endl << endl;
+//    int n;
+//    cout << "Enter size for an array : ";
+//    cin >> n;
+
+//    char array[n];
+
+//    for (int i = 0; i < n; i++) {
+//       char l;
+//       cout << "Enter " << i << "th element : ";
+//       cin >> l;
+//       array[i] = l;
+//    }
+
+//    int hash[26] = {0};
+
+//    for (int k = 0; k < n; k++) {
+//       hash[array[k] - 97] += 1;
+//    }
+
+//    int q;
+//    cout << "Enter number of inputs to get counts : ";
+//    cin >> q;
+
+//    while (q--) {
+//       char ch;
+//       cout << "Enter a lowercase alphabet from your array : ";
+//       cin >> ch;
+
+//       cout << "Appearence of " << ch << " is : " << hash[ch - 97] << endl;
+//    }
+
+//    return 0;
+// }
+
+// This is main function is for uppercase alphabets
 int main() {
-   cout << "NOTE: THIS IS ONLY FOR LOWERCASE ALPHABETS !!" << endl << endl;
+   cout << "NOTE: THIS IS ONLY FOR UPPERCASE ALPHABETS !!" << endl << endl;
    int n;
    cout << "Enter size for an array : ";
    cin >> n;
@@ -19,7 +57,7 @@ int main() {
    int hash[26] = {0};
 
    for (int k = 0; k < n; k++) {
-      hash[array[k] - 97] += 1;
+      hash[array[k] - 65] += 1;
    }
 
    int q;
@@ -28,10 +66,10 @@ int main() {
 
    while (q--) {
       char ch;
-      cout << "Enter a lowercase alphabet from your array : ";
+      cout << "Enter a uppercase alphabet from your array : ";
       cin >> ch;
 
-      cout << "Appearence of " << ch << " is : " << hash[ch - 97] << endl;
+      cout << "Appearence of " << ch << " is : " << hash[ch - 65] << endl;
    }
 
    return 0;
