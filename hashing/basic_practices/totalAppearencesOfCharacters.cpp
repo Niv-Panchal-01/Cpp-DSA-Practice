60109f019b0cc87559718d745d625ebab16d013a
Niv Panchal <nivpanchal2122@gmail.com>
2026-07-02 23:16:05 +0530

    Changed Comments

--- hashing/basic_practices/totalAppearencesOfCharacters.cpp
+++ hashing/basic_practices/totalAppearencesOfCharacters.cpp
@@ -2,43 +2,43 @@
 using namespace std;
 
 // This main function is only valid for uppercase alphabet
-// int main() {
-//    cout << "NOTE: THIS IS ONLY FOR LOWERCASE ALPHABETS !!" << endl << endl;
-//    int n;
-//    cout << "Enter size for an array : ";
-//    cin >> n;
+int main() {
+   cout << "NOTE: THIS IS ONLY FOR LOWERCASE ALPHABETS !!" << endl << endl;
+   int n;
+   cout << "Enter size for an array : ";
+   cin >> n;
 
-//    char array[n];
+   char array[n];
 
-//    for (int i = 0; i < n; i++) {
-//       char l;
-//       cout << "Enter " << i << "th element : ";
-//       cin >> l;
-//       array[i] = l;
-//    }
+   for (int i = 0; i < n; i++) {
+      char l;
+      cout << "Enter " << i << "th element : ";
+      cin >> l;
+      array[i] = l;
+   }
 
-//    int hash[26] = {0};
+   int hash[26] = {0};
 
-//    for (int k = 0; k < n; k++) {
-//       hash[array[k] - 97] += 1;
-//    }
+   for (int k = 0; k < n; k++) {
+      hash[array[k] - 97] += 1;
+   }
 
-//    int q;
-//    cout << "Enter number of inputs to get counts : ";
-//    cin >> q;
+   int q;
+   cout << "Enter number of inputs to get counts : ";
+   cin >> q;
 
-//    while (q--) {
-//       char ch;
-//       cout << "Enter a lowercase alphabet from your array : ";
-//       cin >> ch;
+   while (q--) {
+      char ch;
+      cout << "Enter a lowercase alphabet from your array : ";
+      cin >> ch;
 
-//       cout << "Appearence of " << ch << " is : " << hash[ch - 97] << endl;
-//    }
+      cout << "Appearence of " << ch << " is : " << hash[ch - 97] << endl;
+   }
 
-//    return 0;
-// }
+   return 0;
+}
 
-// This is main function is for uppercase alphabets
+/* This is main function is for uppercase alphabets
 int main() {
    cout << "NOTE: THIS IS ONLY FOR UPPERCASE ALPHABETS !!" << endl << endl;
    int n;
@@ -74,3 +74,4 @@ int main() {
 
    return 0;
 }
+*/
