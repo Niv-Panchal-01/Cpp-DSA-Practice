f6ddbb3263b255a87503e2148f31767f11def8f3
Niv Panchal <nivpanchal2122@gmail.com>
2026-07-04 16:47:56 +0530

    Created it's own  selection sort function

--- sorting/Selection_Sort/bruteForceMethod.cpp
+++ sorting/Selection_Sort/bruteForceMethod.cpp
@@ -2,6 +2,26 @@
 #include <vector>
 using namespace std;
 
+void selectionSort(vector<int> array) {
+   int s = array.size();
+   for (int k = 0; k < s; k++) {
+      int smallestNumberIndex = k;
+      for (int n = k + 1; n < s; n++) {
+         if (array[smallestNumberIndex] > array[n]) {
+            smallestNumberIndex = n;
+         }
+      }
+      swap(array[k], array[smallestNumberIndex]);
+   }
+}
+
+void printArray(vector<int> array) {
+   for (int val : array) {
+      cout << val << ' ';
+   }
+   cout << endl;
+}
+
 int main() {
    int s;
    cout << "Enter size of the array : ";
@@ -14,26 +34,11 @@ int main() {
       cin >> array[i];
    }
 
-   cout << "Before Selection Sort : " << endl;
-   for (int val : array) {
-      cout << val << ' ';
-   }
-
-   // Selection Sort Brute Force Approach
-   for (int k = 0; k < s; k++) {
-      int smallestNumberIndex = k;
-      for (int n = k + 1; n < s; n++) {
-         if (array[smallestNumberIndex] > array[n]) {
-            smallestNumberIndex = n;
-         }
-      }
-      swap(array[k], array[smallestNumberIndex]);
-   }
+   cout << "Before Selection Sort : ";
+   printArray(array);
 
    cout << endl << "After Selection Sort : ";
-   for (int val : array) {
-      cout << val << ' ';
-   }
+   printArray(array);
 
    return 0;
 }
