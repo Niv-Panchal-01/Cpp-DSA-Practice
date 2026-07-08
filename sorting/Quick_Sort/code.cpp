d43f70560344580d7b0236a8efc3c95f4735d1d8
Niv Panchal <nivpanchal2122@gmail.com>
2026-07-08 21:26:28 +0530

    Used recursive approach

---
+++ sorting/Quick_Sort/code.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+void quickSort(vector<int> &arr, int low, int high) {
+   if (low >= high) {
+      return;
+   }
+
+   int pivot = low;
+   int i = low + 1, j = high;
+
+   while (i <= j) {
+      if (arr[pivot] < arr[i]) {
+         if (arr[pivot] > arr[j]) {
+            swap(arr[i], arr[j]);
+         }
+         j--;
+      } else {
+         i++;
+      }
+   }
+
+   swap(arr[pivot], arr[j]);
+
+   quickSort(arr, low, j - 1);
+   quickSort(arr, j + 1, high);
+}
+
+void printArray(vector<int> arr) {
+   for (int val : arr) {
+      cout << val << ' ';
+   }
+   cout << endl;
+}
+
+int main() {
+   int n;
+   cout << "Enter a size to input array : ";
+   cin >> n;
+
+   vector<int> array(n);
+
+   for (int i = 0; i < n; i++) {
+      cout << "Enter " << i << "th element : ";
+      cin >> array[i];
+   }
+
+   cout << "Your array before quick sort : ";
+   printArray(array);
+
+   quickSort(array, 0, n - 1);
+
+   cout << "Your array after quick sort : ";
+   printArray(array);
+   return 0;
+}
