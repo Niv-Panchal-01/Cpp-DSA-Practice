003689ac3d23ecb687b4d0b1538e3422a09aa27a
Niv Panchal <nivpanchal2122@gmail.com>
2026-07-28 20:56:36 +0530

    Optimize single number using XOR

--- vectors/BasicProblems/findNumberThatAppearsOnce.cpp
+++ vectors/BasicProblems/findNumberThatAppearsOnce.cpp
@@ -5,27 +5,23 @@ appears twice except for one. Find that single one.
 */
 
 #include <iostream>
-#include <unordered_map>
 #include <vector>
 using namespace std;
 
 int getNumberThatAppearsOnce(const vector<int> &arr) {
-   unordered_map<int, int> ump;
-   for (const int &val : arr) {
-      ump[val]++;
+   int n = arr.size();
+   int XOR = arr[0];
+
+   for (int i = 1; i < n; i++) {
+      XOR ^= arr[i];
    }
 
-   for (const auto &val : ump) {
-      if (val.second == 1) {
-         return val.first;
-      }
-   }
-
-   return 0;
+   return XOR;
 }
 
 int main() {
-   vector<int> arr = {2, 2, 1, 1, 16, 5, 6, 7, 7, 6, 5, 8, 9, 8, 9};
+   vector<int> arr = {2, 2, 1, 1, 10, 11, 524, 10, 11, 5,
+                      6, 7, 7, 6, 5,  8,  9,   8,  9};
 
    cout << "Here is the number that appears once in your array : "
         << getNumberThatAppearsOnce(arr);
