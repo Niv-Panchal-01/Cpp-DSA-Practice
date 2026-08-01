cc346f8def9162b15c4b5ea6b880ad2e17849e44
Niv Panchal <nivpanchal2122@gmail.com>
2026-08-01 21:24:27 +0530

    Implement prefix sum and unordered_map for all signed integers

--- vectors/BasicProblems/longestSubarrayWithSumK.cpp
+++ vectors/BasicProblems/longestSubarrayWithSumK.cpp
@@ -1,30 +1,36 @@
 #include <iostream>
+#include <unordered_map>
 #include <vector>
 using namespace std;
 
-// only for positive numbers
 int getLongestSubarrayWithsumK(const vector<int> &arr, int k) {
    int n = arr.size();
    int maxSum = 0;
+   int sum = 0;
+   unordered_map<int, int> preSumMap;
 
    for (int i = 0; i < n; i++) {
-      int sum = 0;
-      int counter = 0;
-      for (int j = i; j < n; j++) {
-         sum += arr[j];
-         if (sum == k) {
-            maxSum = max(maxSum, j - i + 1);
-            break;
-         }
+      sum += arr[i];
+
+      if (sum == k) {
+         maxSum = max(maxSum, i + 1);
+      }
+      int rem = sum - k;
+
+      if (preSumMap.find(rem) != preSumMap.end()) {
+         int len = i - preSumMap[rem];
+         maxSum = max(maxSum, len);
+      }
+      if (preSumMap.find(sum) == preSumMap.end()) {
+         preSumMap[sum] = i;
       }
    }
-
    return maxSum;
 }
 
 int main() {
-   vector<int> arr = {10, 5, 2, 7, 1, 9};
+   vector<int> arr = {2, -1, 1, 3};
 
-   cout << "Your answer is : " << getLongestSubarrayWithsumK(arr, 15);
+   cout << "Your answer is : " << getLongestSubarrayWithsumK(arr, 3);
    return 0;
 }
