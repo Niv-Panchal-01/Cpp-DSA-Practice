48e747801aca77c8eccd2b9551f51332a56b2c08
Niv Panchal <nivpanchal2122@gmail.com>
2026-08-10 22:07:54 +0530

    Optimize the solution using Kadane's Algorithm

--- vectors/Leetcode/053_maximumSubArray.cpp
+++ vectors/Leetcode/053_maximumSubArray.cpp
@@ -3,18 +3,20 @@
 using namespace std;
 
 /*
-Time Complexity : O(n ^ 2)
+Time Complexity : O(n)
 Space Complexity : O(1)
 */
 
 int maxSubArray(const vector<int> &nums) {
    int n = nums.size();
    int maxSum = INT32_MIN;
+   int currentSum = 0;
    for (int i = 0; i < n; i++) {
-      int sum = 0;
-      for (int j = i; j < n; j++) {
-         sum += nums[j];
-         maxSum = max(maxSum, sum);
+      currentSum += nums[i];
+      maxSum = max(maxSum, currentSum);
+
+      if (currentSum < 0) {
+         currentSum = 0;
       }
    }
 
