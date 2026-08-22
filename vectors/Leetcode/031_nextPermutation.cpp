b3e02ba2cf60a80cfb7b07d3519f1e76fba4ec85
Niv Panchal <nivpanchal2122@gmail.com>
2026-08-22 17:29:42 +0530

    Optimize the solution

--- vectors/Leetcode/031_nextPermutation.cpp
+++ vectors/Leetcode/031_nextPermutation.cpp
@@ -1,25 +1,46 @@
+#include <algorithm>
 #include <iostream>
 #include <vector>
-#include <algorithm>
 using namespace std;
 
-vector<int> nextPermutation(vector<int> &arr){
-   next_permutation(arr.begin(), arr.end());
+vector<int> nextPermutation(vector<int> &arr) {
+   int n = arr.size();
+   int index = -1;
+
+   for (int i = n - 2; i >= 0; i--) {
+      if (arr[i] < arr[i + 1]) {
+         index = i;
+         break;
+      }
+   }
+
+   if (index == -1) {
+      reverse(arr.begin(), arr.end());
+      return arr;
+   }
+
+   for (int i = n - 1; i >= index; i--) {
+      if (arr[index] < arr[i]) {
+         swap(arr[i], arr[index]);
+         break;
+      }
+   }
+
+   reverse(arr.begin() + index + 1, arr.end());
 
    return arr;
 }
 
-void printArray(const vector<int> &arr){
-   for(int val : arr){
+void printArray(const vector<int> &arr) {
+   for (int val : arr) {
       cout << val << ' ';
    }
 
    cout << endl;
 }
 
-
-int main(){
-   vector<int> arr = {2,1,5,4,3,0,0};
+int main() {
+   vector<int> arr = {2, 1, 5, 4, 3, 0, 0};
 
    cout << "Next permutation of your array : ";
    printArray(nextPermutation(arr));
