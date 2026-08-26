0961747379637bd26f5ec73fbf6a3baacaf298d9
Niv Panchal <nivpanchal2122@gmail.com>
2026-08-26 22:17:17 +0530

    Change second iteration from using array to numsSet

--- vectors/Leetcode/128_longestConsecutiveSequence.cpp
+++ vectors/Leetcode/128_longestConsecutiveSequence.cpp
@@ -24,9 +24,9 @@ int longestConsecutiveSequence(const vector<int> &arr) {
    }
 
    //Time complexity : O(2n)
-   for(int i = 0; i < n; i++){
-      if(numsSet.find(arr[i] - 1) == numsSet.end()){
-         int element = arr[i];
+   for(int val : numsSet){
+      if(numsSet.find(val - 1) == numsSet.end()){
+         int element = val;
          while(numsSet.find(element + 1) != numsSet.end()){
             currentSequence++;
             maxSequence = max(maxSequence, currentSequence);
@@ -41,8 +41,8 @@ int longestConsecutiveSequence(const vector<int> &arr) {
 }
 
 int main() {
-   vector<int> arr = {102, 4, 100, 1, 101, 3, 2, 1, 1};
-   // vector<int> arr = {1, 0, 1, 2};
+   // vector<int> arr = {102, 4, 100, 1, 101, 3, 2, 1, 1};
+   vector<int> arr = {1, 0, 1, 2};
    // vector<int> arr = {0, 3, 7, 2, 5, 8, 4, 6, 0, 1};
    // vector<int> arr = {100, 4, 200, 1, 3, 2};
 
