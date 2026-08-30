8bf763b44f041820620270263e74143baa41fbbf
Niv Panchal <nivpanchal2122@gmail.com>
2026-08-30 13:21:28 +0530

    Optimize the solution doing transpose the matrix and reverse each rows

--- vectors/Leetcode/048_rotateImage.cpp
+++ vectors/Leetcode/048_rotateImage.cpp
@@ -1,30 +1,26 @@
 #include <iostream>
 #include <vector>
+#include <algorithm>
 using namespace std;
 
 /*
 Time Complexity : O(n ^ 2)
-Space Complexity : O(n ^ 2)
+Space Complexity : O(1)
 */
 
 void rotate(vector<vector<int>> &matrix) {
    int n = matrix.size();
-   vector<vector<int>> temp(n, vector<int>(n));
-   int indexOfColumn = 0;
 
    for (int i = 0; i < n; i++) {
-      int indexOfRow = n - 1;
-      for (int j = 0; j < n; j++) {
-         temp[i][j] = matrix[indexOfRow][indexOfColumn];
-         indexOfRow--;
+      for (int j = i + 1; j < n; j++) {
+         if(i != j){
+            swap(matrix[i][j], matrix[j][i]);
+         }
       }
-      indexOfColumn++;
    }
 
    for (int i = 0; i < n; i++) {
-      for (int j = 0; j < n; j++) {
-         matrix[i][j] = temp[i][j];
-      }
+      reverse(matrix[i].begin(), matrix[i].end());
    }
 }
 
@@ -42,9 +38,9 @@ void printMatrix(const vector<vector<int>> &matrix) {
 
 int main() {
 
-   // vector<vector<int>> matrix = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
-   vector<vector<int>> matrix = {
-       {5, 1, 9, 11}, {2, 4, 8, 10}, {13, 3, 6, 7}, {15, 14, 12, 16}};
+   vector<vector<int>> matrix = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+   // vector<vector<int>> matrix = {
+      //  {5, 1, 9, 11}, {2, 4, 8, 10}, {13, 3, 6, 7}, {15, 14, 12, 16}};
 
    cout << "Matrix before 90 Degree ratation : " << endl;
    printMatrix(matrix);
