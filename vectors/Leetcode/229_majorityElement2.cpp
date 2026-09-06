0bbc100ee2224e68d1c0854f2ba0263e612518bb
Niv Panchal <nivpanchal2122@gmail.com>
2026-09-06 14:10:57 +0530

    Implemented slightly better solution using sorting algorithm

--- vectors/Leetcode/229_majorityElement2.cpp
+++ vectors/Leetcode/229_majorityElement2.cpp
@@ -1,28 +1,52 @@
 #include <iostream>
 #include <unordered_map>
 #include <vector>
+#include <algorithm>
 using namespace std;
 
 /*
-Time Complexity: O(n)
-Space Complexity: O(n)
+Time Complexity: O(n log n)
+Space Complexity: O(1)
 */
 
 vector<int> majorityElement(vector<int> &nums) {
    int n = nums.size();
    vector<int> answer;
-   unordered_map<int, int> uMap;
 
-   for (int i = 0; i < n; i++) {
-      int Element = nums[i];
-      uMap[Element]++;
+   sort(nums.begin(), nums.end());
+   int element = nums[0];
+   int count = 1;
+   int eleCount = 1;
+
+   if (n == 1) {
+      answer.push_back(element);
+      return answer;
+   } else if (n == 2) {
+      answer.push_back(element);
+      if (element != nums[1]) {
+         answer.push_back(nums[1]);
+      }
+      return answer;
    }
 
-   for (int i = 0; i < n; i++) {
-      int Element = nums[i];
-      if (uMap[Element] > n / 3) {
-         answer.push_back(Element);
-         uMap[Element] = 0;
+   for (int i = 1; i < n; i++) {
+      if (count == 0) {
+         i--;
+         element = nums[i];
+      }
+
+      if (element == nums[i]) {
+         count++;
+      } else if (count > n / 3) {
+         answer.push_back(element);
+         count = 0;
+      } else {
+         count = 0;
+         eleCount = 1;
+      }
+
+      if (count > n / 3 && i == n - 1) {
+         answer.push_back(element);
       }
    }
 
@@ -37,7 +61,7 @@ void printArray(vector<int> nums) {
 }
 
 int main() {
-   vector<int> nums = {3,3,4};
+   vector<int> nums = {3, 3, 4};
    printArray(majorityElement(nums));
    return 0;
 }
